2f4d573619262617181b666dfd64a88b77d8dd7e
Zachary Argentin <zargentin@gmail.com>
2023-06-02 22:06:05 -0700

    Clicking a box now results in an x being drawn on the gameboard, and the game state is updated in the backend

--- game.js
+++ game.js
@@ -5,8 +5,27 @@ let gameState = ["", "", "", "", "", "", "", "", "", ]
 
 // document.querySelector(".box").addEventListener("click", boxClicked);
 const boxes = document.querySelectorAll(".box");
-boxes.forEach(box => addEventListener("click", boxClicked));
+boxes.forEach(box => addEventListener("click", makeMove));
 
-function boxClicked() {
-    console.log("Click!");
+function makeMove(clickEvent) {
+    const boxClicked = clickEvent.target;  // clickEvent.target returns the HTML element that was clicked
+    const boxIndex = parseInt(boxClicked.dataset.index);  // .dataset retrieves an HTML attribute prefixed with "data-". In this case, we retrieve the "data-index" attribute.
+    console.log(boxIndex);
+
+    if (gameState[boxIndex] !== "" || !gameActive) {
+        return;
+    }
+
+    updateGame(boxClicked, boxIndex);
+    checkVictory();
+}
+
+function updateGame(boxClicked, boxIndex) {
+    gameState[boxIndex] = currentPlayer;
+    boxClicked.innerHTML = currentPlayer;
+    console.log(gameState);
+}
+
+function checkVictory() {
+    return;
 }
