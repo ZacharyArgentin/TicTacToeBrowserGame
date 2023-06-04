89e649c9ae9fe5804595c788241a12d7a11871d9
Zachary Argentin <zargentin@gmail.com>
2023-06-04 14:43:32 -0700

    Added a win condition check and a win message when the player wins

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 const statusDisplay = document.querySelector(".game-status");
 let gameActive = true;
-let currentPlayer = "x";
+let currentPlayer = "X";
 let gameState = ["", "", "", "", "", "", "", "", "", ]
 
 // document.querySelector(".box").addEventListener("click", boxClicked);
@@ -17,7 +17,7 @@ function makeMove(clickEvent) {
     }
 
     updateGame(boxClicked, boxIndex);
-    checkVictory();
+    checkVictory(gameState);
 }
 
 function updateGame(boxClicked, boxIndex) {
@@ -26,6 +26,28 @@ function updateGame(boxClicked, boxIndex) {
     console.log(gameState);
 }
 
-function checkVictory() {
+function checkVictory(gameState) {
+    let gameWon = false;
+
+    // Check all 8 possible "3 in a row" combinations
+    // Check horizontal "3 in a rows"
+    if      ( (gameState[0] == gameState[1] && gameState[1] == gameState[2]) && (gameState[0] != "") ) {gameWon = true;}
+    else if ( (gameState[3] == gameState[4] && gameState[4] == gameState[5]) && (gameState[3] != "") ) {gameWon = true;}
+    else if ( (gameState[6] == gameState[7] && gameState[7] == gameState[8]) && (gameState[6] != "") ) {gameWon = true;}
+    // Check vertical "3 in a rows"
+    else if ( (gameState[0] == gameState[3] && gameState[3] == gameState[6]) && (gameState[0] != "") ) {gameWon = true;}
+    else if ( (gameState[1] == gameState[4] && gameState[4] == gameState[7]) && (gameState[1] != "") ) {gameWon = true;}
+    else if ( (gameState[2] == gameState[5] && gameState[5] == gameState[8]) && (gameState[2] != "") ) {gameWon = true;}
+    // Check diagonal "3 in a rows"
+    else if ( (gameState[0] == gameState[4] && gameState[4] == gameState[8]) && (gameState[0] != "") ) {gameWon = true;}
+    else if ( (gameState[2] == gameState[4] && gameState[4] == gameState[6]) && (gameState[2] != "") ) {gameWon = true;}
+
+    // Activate end of game sequence
+    if (gameWon) {
+        gameActive = false;
+        statusDisplay.innerHTML = `Player ${currentPlayer} Wins!`
+        console.log(`Player ${currentPlayer} Wins!`);
+    }
+
     return;
 }
