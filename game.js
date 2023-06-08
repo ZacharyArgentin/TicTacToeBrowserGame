690e1b01fdd802e070faf27c62b11becc10693c0
Zachary Argentin <zargentin@gmail.com>
2023-06-07 23:35:09 -0700

    Added some logging and a restart game function

--- game.js
+++ game.js
@@ -1,33 +1,47 @@
 const statusDisplay = document.querySelector(".game-status");
 let gameActive = true;
-let gameWon = false;
 let currentPlayer = "X";
 let gameState = ["", "", "", "", "", "", "", "", "", ]
 
-// document.querySelector(".box").addEventListener("click", boxClicked);
+// Add event listeners
 const boxes = document.querySelectorAll(".box");
 boxes.forEach(box => addEventListener("click", makeMove));
+document.querySelector(".game-restart").addEventListener("click", restartGame);
 
+console.log("Game Start");
+statusDisplay.innerHTML = `It's ${currentPlayer}'s turn`
+
+// Why does this function run when clicking literally anywhere? Rather than only when clicking a box?
 function makeMove(clickEvent) {
+    console.log("Running makeMove function");
     const boxClicked = clickEvent.target;  // clickEvent.target returns the HTML element that was clicked
     const boxIndex = parseInt(boxClicked.dataset.index);  // .dataset retrieves an HTML attribute prefixed with "data-". In this case, we retrieve the "data-index" attribute.
-    console.log(boxIndex);
-
+    
     if (gameState[boxIndex] !== "" || !gameActive) {
         return;
+    } else {
+        console.log(`${currentPlayer} played a move in box ${boxIndex}`);
     }
 
     updateGame(boxClicked, boxIndex);
     checkVictory(gameState);
-    if (gameActive) {switchPlayer();}
+    console.log(`Is the game still active? ${gameActive}`);
+    if (gameActive) {
+        switchPlayer();
+        console.log(`Player ${currentPlayer}'s turn:`);
+    }
 }
 
 function updateGame(boxClicked, boxIndex) {
+    console.log("Running updateGame function");
     gameState[boxIndex] = currentPlayer;
     boxClicked.innerHTML = currentPlayer;
 }
 
 function checkVictory(gameState) {
+    console.log("Running checkVictory function");
+    let gameWon = false;
+    let isStalemate = false;
     // Check all 8 possible "3 in a row" combinations
     // Check horizontal "3 in a rows"
     if      ( (gameState[0] == gameState[1] && gameState[1] == gameState[2]) && (gameState[0] != "") ) {gameWon = true;}
@@ -42,12 +56,8 @@ function checkVictory(gameState) {
     else if ( (gameState[2] == gameState[4] && gameState[4] == gameState[6]) && (gameState[2] != "") ) {gameWon = true;}
 
     // Check for a stalemate
-    let isStalemate = true;
-    for (i = 0; i < gameState.length; i++) {
-        if (gameState[i] === "") {
-            isStalemate = false;
-        }
-    }
+    if (!gameWon) {isStalemate = !gameState.includes("");}
+    
 
     // Activate end of game sequence
     if (gameWon) {
@@ -62,6 +72,7 @@ function checkVictory(gameState) {
 }
 
 function switchPlayer() {
+    console.log("Running switchPlayer function");
     if (currentPlayer === "X") {
         currentPlayer = "O";
     } else {
@@ -69,4 +80,13 @@ function switchPlayer() {
     }
 
     statusDisplay.innerHTML = `It's ${currentPlayer}'s turn`
+}
+
+function restartGame() {
+    console.log("Restarting game...")
+    gameState = ["", "", "", "", "", "", "", "", "", ];
+    currentPlayer = "X";
+    gameActive = true;
+    document.querySelectorAll(".box").forEach(box => box.innerHTML = "");
+    statusDisplay.innerHTML = `It's ${currentPlayer}'s turn`;
 }
